d412e4821b4dd45dd876fe7d5283791a3b017d7c
Ramiro Andrés <ramiroandres@Ramiros-MacBook-Pro.local>
2019-01-03 19:10:37 -0500

    Updates

---
+++ precache-manifest.7125987044835f79f63643cee1dba09e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "6645a114b3e9eab5b444",
+    "url": "/weather-app/static/js/main.6645a114.chunk.js"
+  },
+  {
+    "revision": "8024c95ba9cd7bd49d49",
+    "url": "/weather-app/static/js/1.8024c95b.chunk.js"
+  },
+  {
+    "revision": "6645a114b3e9eab5b444",
+    "url": "/weather-app/static/css/main.4c58d393.chunk.css"
+  },
+  {
+    "revision": "8a0437b70f3d089c4f07176402eeb8bb",
+    "url": "/weather-app/index.html"
+  }
+];
